5b5a8b5ef4b9da60c94dbe3144ed726d0f398835
yukiii <edyardrix@gmail.com>
2025-03-23 18:21:42 +0300

    Добавьте файлы проекта.

---
+++ RatingMeApp/AdminWindow.xaml.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RatingMeApp.Windows
+{
+    public partial class AdminWindow : Window
+    {
+        //VisitorContext db = new VisitorContext();
+        public AdminWindow()
+        {
+            InitializeComponent();
+
+            Loaded += UserEditWindow_Loaded;
+        }
+
+        private void UserEditWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            //db.Database.EnsureCreated();
+            //db.Visitors.Load();
+            //DataContext = db.Visitors.Local.ToObservableCollection();
+        }
+
+        private void AddVisitor_Click(object sender, RoutedEventArgs e)
+        {
+            //DialogUserWindow DialogUserWindow = new DialogUserWindow(new Visitor());
+            //if (DialogUserWindow.ShowDialog() == true)
+            //{
+            //    Visitor Visitor = DialogUserWindow.Visitor;
+            //    db.Visitors.Add(Visitor);
+            //    db.SaveChanges();
+            //}
+        }
+
+        private void EditVisitor_Click(object sender, RoutedEventArgs e)
+        {
+            //Visitor? visitor = visitorsList.SelectedItem as Visitor;
+            //if (visitor is null) return;
+
+            //DialogUserWindow DialogUserWindow = new DialogUserWindow(new Visitor
+            //{
+            //    Id = visitor.Id,
+            //    Fio = visitor.Fio,
+            //    Pas = visitor.Pas
+            //});
+
+            //if (DialogUserWindow.ShowDialog() == true)
+            //{
+            //    visitor = db.Visitors.Find(DialogUserWindow.Visitor.Id);
+            //    if (visitor != null)
+            //    {
+            //        visitor.Fio = DialogUserWindow.Visitor.Fio;
+            //        visitor.Pas = DialogUserWindow.Visitor.Pas;
+            //        db.SaveChanges();
+            //        visitorsList.Items.Refresh();
+            //    }
+            //}
+
+            //User? user = usersList.SelectedItem as User;
+            //// если ни одного объекта не выделено, выходим
+            //if (user is null) return;
+
+            //UserWindow UserWindow = new UserWindow(new User
+            //{
+            //    Id = user.Id,
+            //    Age = user.Age,
+            //    Name = user.Name
+            //});
+
+            //if (UserWindow.ShowDialog() == true)
+            //{
+            //    // получаем измененный объект
+            //    user = db.Users.Find(UserWindow.User.Id);
+            //    if (user != null)
+            //    {
+            //        user.Age = UserWindow.User.Age;
+            //        user.Name = UserWindow.User.Name;
+            //        db.SaveChanges();
+            //        usersList.Items.Refresh();
+            //    }
+            //}
+        }
+        // удаление
+        private void DeleteVisitor_Click(object sender, RoutedEventArgs e)
+        {
+            //Visitor? visitor = visitorsList.SelectedItem as Visitor;
+            //if (visitor is null) return;
+            //db.Visitors.Remove(visitor);
+            //db.SaveChanges();
+            //// получаем выделенный объект
+            //User? user = usersList.SelectedItem as User;
+            //// если ни одного объекта не выделено, выходим
+            //if (user is null) return;
+            //db.Users.Remove(user);
+            //db.SaveChanges();
+        }
+    }
+}
---
+++ RatingMeApp/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RatingMeApp.Windows;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RatingMeApp
+{
+
+    public partial class MainWindow : Window
+    {
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void MainClose_CLick(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        public void Admin_Click(object sender, EventArgs e)
+        {
+            //ShowBaseWindow showBaseWindow = new ShowBaseWindow();
+            //showBaseWindow.Show();
+
+            AdminWindow adminWindow = new AdminWindow();
+            adminWindow.ShowDialog();
+
+            //DialogUserEditWindow dialogUserEditWindow = new DialogUserEditWindow();
+            //dialogUserEditWindow.ShowDialog();
+
+            //DialogUserWindow DialogUserWindow = new DialogUserWindow(new Visitor());
+            //if (DialogUserWindow.ShowDialog() == true)
+            //{
+            //    Visitor Visitor = DialogUserWindow.Visitor;
+            //    db.Visitors.Add( Visitor );
+            //    db.SaveChanges();
+            //}
+
+            //using (AppContext db = new AppContext())
+            //{
+            //    var users = db.Users.Where(p => p.Fio == FIO.Text);
+            //    foreach (User user in users)
+            //        if (users != null)
+            //        {
+            //            ShowBaseWindow showBaseWindow = new ShowBaseWindow();
+            //            showBaseWindow.Show();
+            //        }
+            //}
+        }
+
+        public void ClearFio_Click(object sender, EventArgs e)
+        {
+            FIO.Clear();
+        }
+
+        public void ClearPassword_Click(object sender, EventArgs e)
+        {
+            PAS.Clear();
+        }
+
+        public void FocusCheck(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            if (FIO.Text.Length == 0)
+                FIO.Text = "Введите ФИО";
+            if (PAS.Text.Length == 0)
+                PAS.Text = "Введите пароль";
+        }
+
+        public void Info_Click(object sender, EventArgs e)
+        {
+            //MainInfoWindow mainInfoWindow = new MainInfoWindow();
+            //mainInfoWindow.ShowDialog();
+        }
+
+        private void StudentEnter_Click(object sender, RoutedEventArgs e)
+        {
+            //StudentMainWindow studentMainWindow = new StudentMainWindow();
+            //studentMainWindow.Show();
+            //this.Close();
+        }
+    }
+}
